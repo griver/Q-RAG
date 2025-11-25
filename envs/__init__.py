2eee6b1092c34110b06701234b009e4bbf179e6f
Oleg Inozemcev <oleg0x@yandex.ru>
2025-11-25 17:48:13 +0300

    Implement GSM8K dataset support

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,3 +1,3 @@
-from .dataloaders import RetrievalBabiLong, RetrievalMusique, RetrievalHotPotQA, RetrievalLongBench, GlobalSet, NIAH
+from .dataloaders import RetrievalBabiLong, RetrievalMusique, RetrievalHotPotQA, RetrievalLongBench, GlobalSet, NIAH, RetrievalGSM8K
 from .qa_dataset_adapter import QADatasetAdapter
 from .combined_dataset import RetrievalCombinedTwo, RetrievalCombinedThree
